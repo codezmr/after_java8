3d4d3bf293ef254b291cb354a0b18ab839b06509
mdzam <mdzam@ZMR>
2022-07-02 11:24:22 +0530

    main method in interface

--- Java8Features/src/com/codezmr/interface_enhancement/staticmethod/StatticMethodDemo.java
+++ Java8Features/src/com/codezmr/interface_enhancement/staticmethod/StatticMethodDemo.java
@@ -9,4 +9,10 @@ public interface StatticMethodDemo {
 	public static void show2(){ //static method do not available in implementing classes.
 		System.out.println("I am a Java 8 Static method");
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("From java 8 onward main method can be in interface also");
+	}
 }
+
+
