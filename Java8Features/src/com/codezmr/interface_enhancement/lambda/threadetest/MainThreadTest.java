3c5c2b9b8a8036aee4368e37386f06958e99e2b6
mdzam <mdzam@ZMR>
2022-07-09 16:54:26 +0530

    lambda function

--- Java8Features/src/com/codezmr/interface_enhancement/lambda/threadetest/MainThreadTest.java
+++ Java8Features/src/com/codezmr/interface_enhancement/lambda/threadetest/MainThreadTest.java
@@ -4,7 +4,16 @@ public class MainThreadTest {
 
 	public static void main(String[] args) {
 
-		Runnable runnable = new TestThreadLambda();
+		//Runnable runnable = new TestThreadLambda();
+		
+		Runnable runnable = ()->{
+			
+			for (Integer count = 1; count <= 5; count++) {
+
+				System.out.println("I am in run Method: " + count);
+
+			}
+		};
 		
 		Thread thread1 = new Thread(runnable);
 
