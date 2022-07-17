52f578cf90722d476a4268ba59bc59b7ff90e104
mdzam <mdzam@ZMR>
2022-07-17 09:31:53 +0530

    merge filter and map

--- Java8Features/src/streamsPkg/StreamMapFilter.java
+++ Java8Features/src/streamsPkg/StreamMapFilter.java
@@ -7,20 +7,26 @@ import java.util.stream.Collectors;
 public class StreamMapFilter {
 
 	public static void main(String[] args) {
-		
+
 		List<String> fruits = new ArrayList<String>();
 		fruits.add("Mango");
-		fruits.add("Banana");
+		fruits.add("Bananas");
 		fruits.add("Apple");
 		fruits.add("Kiwi");
-		fruits.add("Papaya");
+		fruits.add("Papayas");
 		fruits.add("Grapes");
+
+		// find only those fruits whose length is greater than 5.
+
+		System.out.println("Length of Fruits are : " 
+				+ fruits.stream().map(f -> f.length()).collect(Collectors.toList()));
 		
-		//find only those fruits whose length is greater than 5.
+		System.out.println("Fruits whose length is greater than 5 : "
+				+ fruits.stream().filter(f -> f.length() > 5).collect(Collectors.toList()));
 		
-		System.out.println(fruits.stream().map(f -> f.length()).collect(Collectors.toList()));
-		System.out.println(fruits.stream().filter(f -> f.length() > 5).collect(Collectors.toList()));
-		
+		System.out.println("Length of fruits whose length is greater than 5 : "
+				+ fruits.stream().filter(f -> f.length() > 5).map(l -> l.length()).collect(Collectors.toList()));
+
 	}
 
 }
