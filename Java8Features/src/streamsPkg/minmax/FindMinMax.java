4592d9627ac02b3c041100b301cd2d1e6a1e264e
mdzam <mdzam@ZMR>
2022-07-17 10:35:39 +0530

    Find Min Max using colon operator

--- Java8Features/src/streamsPkg/minmax/FindMinMax.java
+++ Java8Features/src/streamsPkg/minmax/FindMinMax.java
@@ -10,11 +10,22 @@ public class FindMinMax {
 		
 		List<Integer> numbers = Arrays.asList(22,4,33,53,82,19,73);
 		
-		Integer minVal = numbers.stream().min((a, b)-> a.compareTo(b)).get();
-		Integer maxVal = numbers.stream().max((a, b)-> a.compareTo(b)).get();
-
+		/*
+		 * Integer minVal = numbers.stream().min((a, b)-> a.compareTo(b)).get(); Integer
+		 * maxVal = numbers.stream().max((a, b)-> a.compareTo(b)).get();
+		 */
+		
+		Integer minVal = numbers.stream().min(FindMinMax :: sortElements).get();
+		Integer maxVal = numbers.stream().max(FindMinMax :: sortElements).get();
+		
 		System.out.println("Minimum Number is: "+ minVal + "\nMaximum number is: "+ maxVal);
+
+
+	}
 	
+	public static Integer sortElements(Integer a, Integer b) {
+		
+		return a.compareTo(b);
 	}
 
 }
